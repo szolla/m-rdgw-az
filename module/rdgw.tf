aa91b9314074c2f7bcab0ed8e639f7835919887f
szolla <35842149+szolla@users.noreply.github.com>
2019-02-19 10:55:49 -0600

    Update rdgw.tf

--- module/rdgw.tf
+++ module/rdgw.tf
@@ -51,7 +51,7 @@ resource "azurerm_virtual_machine" "rdgw_host" {
   os_profile {
     computer_name  = "hostname"
     admin_username = "rduser"
-    admin_password = ""
+    admin_password = "${var.adminpass}"
   }
   os_profile_windows_config {
     enable_automatic_upgrades = true
